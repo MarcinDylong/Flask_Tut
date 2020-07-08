fcb5649ecd02db81822df9b132e38ec632a0574c
Marcin Dylong <marcindylong@gmail.com>
2020-07-08 19:12:29 +0200

    Generating links

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
+
 from app import app
 from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -8,21 +10,22 @@ def index():
     user = {'username': 'Marcin'}
     posts = [
         {
-            'author' : {'username':'Jan'},
-            'body'   : 'Beautiful day in Cracow!'
+            'author': {'username': 'Jan'},
+            'body': 'Beautiful day in Cracow!'
         },
         {
-            'author' : {'username':'Anna'},
-            'body'   : 'Jojorabit is great movie!'
+            'author': {'username': 'Anna'},
+            'body': 'Jojorabit is great movie!'
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login', methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash(f'Login requested for user {form.username.data}, '
               f'remember_me={form.remember_me.data}')
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
