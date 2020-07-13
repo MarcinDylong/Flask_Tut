4026a95fcd9f71baf8bb58d19c174e24e68cc82c
Marcin Dylong <marcindylong@gmail.com>
2020-07-13 20:11:44 +0200

    Translations added

--- app/__init__.py
+++ app/__init__.py
@@ -59,5 +59,5 @@ if not app.debug:
 
 @babel.localeselector
 def get_locale():
-    # return request.accept_languages.best_match(app.config['LANGUAGES'])
-    return 'en'
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    # return 'en'
